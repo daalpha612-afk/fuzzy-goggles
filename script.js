107c5b1f08b7dbd95e32e0a34358db2f4e310854
daalpha612-afk <daalpha612@gmail.com>
2025-10-26 20:41:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function setup() {
+  createCanvas(950, 400);
+}
+
+function draw() {
+  background("white");
+  text("game sorta", 200, 200);
+  noStroke();
+  fill("black")
+  circle(mouseX, mouseY, 50);
+  
+}
+
 
